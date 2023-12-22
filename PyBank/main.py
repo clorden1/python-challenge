25cdf7e32ce9c75799d2ce705174808c7edf3b9e
clorden1 <cpordenla@gmail.com>
2023-12-22 11:45:08 -0600

    header/ row count/ total profit

--- PyBank/main.py
+++ PyBank/main.py
@@ -10,3 +10,13 @@ budgetPath = os.path.join("resources", "budget_data.csv")
 #read budget csv
 with open(budgetPath, "r") as budgetCsv:
     budgetRead = csv.reader(budgetCsv, delimiter=",")
+
+    #read header row
+    header = next(budgetRead)
+
+    #loop through csv
+    count = 0
+    total = 0
+    for rows in budgetRead:
+        count = count + 1    #count of nonheader rows
+        total = total + int(rows[1]) # sum of profits/losses
