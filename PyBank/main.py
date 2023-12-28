abcf4a722d40d26dfc7f002913bb0c5f6e727f8b
clorden1 <cpordenla@gmail.com>
2023-12-28 12:34:20 -0600

    average change, max inc, max dec, and troubleshooting date

--- PyBank/main.py
+++ PyBank/main.py
@@ -8,15 +8,34 @@ import csv
 budgetPath = os.path.join("resources", "budget_data.csv")
 
 #read budget csv
-with open(budgetPath, "r") as budgetCsv:
+with open(budgetPath) as budgetCsv:
     budgetRead = csv.reader(budgetCsv, delimiter=",")
 
     #read header row
     header = next(budgetRead)
 
-    #loop through csv
-    count = 0
-    total = 0
-    for rows in budgetRead:
-        count = count + 1    #count of nonheader rows
-        total = total + int(rows[1]) # sum of profits/losses
+    #create dictionary for profits/losses data
+    dictionary = {rows[0]:int(rows[1]) for rows in budgetRead}
+    profits = list(dictionary.values())
+
+    #initiate variables needed for loops
+    total = sum(dictionary.values())
+    count = len(dictionary)
+    change = 0
+    lsChange = []
+
+    #loop to create list of changes in profits/losses
+    for x in range(1,count):
+        change = profits[x] - profits[x-1]
+        lsChange.append(change)
+
+    #calculate max profit, max loss, and avg profit/loss and assign to variables
+    avgChange = sum(lsChange)/len(lsChange)
+    maxinc = max(lsChange)
+    maxdec = min(lsChange)
+    maxdate = next(list(dictionary.keys())[lsChange.index(maxinc)])
+    mindate = next(list(dictionary.keys())[lsChange.index(maxdec)])
+    
+    print(count,total,avgChange,maxdec,maxinc)
+    print(maxdate)
+    print(mindate)
