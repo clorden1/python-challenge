f9ba30d25fbda5c97f56e8a8307d9dfa9f86fd93
clorden1 <cpordenla@gmail.com>
2024-01-08 09:00:19 -0600

    final pypoll results

---
+++ PyPoll/main.py
@@ -0,0 +1,85 @@
+#import necessary packages
+import os
+import csv
+
+#import csv using realtive path. Directory is the Pypoll folder
+path = os.path.join("Resources","election_data.csv")
+
+#read csv
+with open(path) as pollcsv:
+    readcsv = csv.reader(pollcsv, delimiter = ",")
+
+    #read header row
+    head = next(readcsv)
+
+    #initaite lists for for loop
+    cand = []
+
+    #use for loop to store data in lists
+    for rows in readcsv:
+        cand.append(rows[2])
+
+    #find number of votes by measure length of candidate list
+    total = len(cand)
+    
+    #use the set() function to create a list of all unique names in the candidate list
+    unique_cand = [set(cand)]
+
+    #exploratory test to display all candidates to determine variables for next loop
+    #print(unique_cand)
+
+    #declare variables to store candidate vote counts
+    charles_count = 0
+    diana_count = 0
+    raymon_count = 0
+
+    #for loop do find number of votes for each candidate
+    for x in cand:
+        if x == "Charles Casper Stockham":
+            charles_count += 1
+        elif x == "Diana DeGette":
+            diana_count += 1
+        else:
+            raymon_count += 1
+    
+    #Calculate the percentages of vote recieved by each candidate
+    percChar = round((charles_count/total)*100,3)
+    percDian = round((diana_count/total)*100,3)
+    percRaym = round((raymon_count/total)*100,3)
+
+    #Determine which candidate won the election
+    if percChar > percDian and percChar > percRaym:
+        winner = "Charles Casper Stockham"
+    elif percDian > percChar and percDian > percRaym:
+        winner = "Diana DeGette"
+    else:
+        winner = "Raymon Anthony Doane"
+    
+#print results in terminal
+print(
+f""" Election Results
+ -------------------------
+ Total Votes: {total}
+ -------------------------
+ Charles Casper Stockham: {percChar}% ({charles_count})
+ Diana Degette: {percDian}% ({diana_count})
+ Raymon Anthony Doane: {percRaym}% ({raymon_count})
+ -------------------------
+ Winner : {winner}
+ -------------------------"""
+)
+
+#write results to a .txt file in the analysis folder
+with open("analysis/PyPollResults.txt", "w") as text:
+    text.write(
+f""" Election Results
+ -------------------------
+ Total Votes: {total}
+ -------------------------
+ Charles Casper Stockham: {percChar}% ({charles_count})
+ Diana Degette: {percDian}% ({diana_count})
+ Raymon Anthony Doane: {percRaym}% ({raymon_count})
+ -------------------------
+ Winner : {winner}
+ -------------------------"""
+)
